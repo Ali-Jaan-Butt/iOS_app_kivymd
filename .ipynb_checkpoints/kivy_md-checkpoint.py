8f69b9935cb501cb82174bd57daa359d215912c3
Ali Jaan <129405358+Ali-Jaan-Butt@users.noreply.github.com>
2024-03-07 03:42:02 +0500

    kivymd

---
+++ .ipynb_checkpoints/kivy_md-checkpoint.py
@@ -0,0 +1,63 @@
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import requests
+import json
+
+class StartScreen(Screen):
+    pass
+
+class SignupScreen(Screen):
+    pass
+
+class LoginScreen(Screen):
+    pass
+
+class HomeScreen(Screen):
+    pass
+
+class MyApp(MDApp):
+    cred = credentials.Certificate("doctors-72cd1-firebase-adminsdk-pf7qv-7ca42b4bcf.json")
+    firebase_admin.initialize_app(cred, {'databaseURL':'https://doctors-72cd1-default-rtdb.firebaseio.com'})
+    ref = db.reference('/')
+    def build(self):
+        return Builder.load_file('main.kv')
+
+    def create_patch(self, namee, email, passw):
+        data = {'name':namee, 'email':email, 'password':passw}
+        new_data_ref = self.ref.push(data)
+        
+    def get_sign(self, email, password):
+        data = self.ref.get()
+        self.error_label = Label(text='', color=(1, 0, 0, 1))
+        self.error_label.text = "Invalid username or password"
+        popup = Popup(title='Login Error', content=self.error_label, size_hint=(None, None), size=(400, 200))
+        x = 1
+        for i in data:
+            if data[i]['email']==email and data[i]['password']==password:
+                app = MDApp.get_running_app()
+                app.change_screen('home_screen')
+                x+=1
+                break
+            else:
+                popup = popup
+        if x==1:
+            popup.open()
+        else:
+            pass
+    
+    def change_screen(self, screen):
+        screen_manager = self.root.ids['screen_manager']
+        screen_manager.current = screen
+
+
+if __name__ == '__main__':
+    MyApp().run()
